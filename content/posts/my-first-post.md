9441b70b0d386dbc94821e351ffd39f6e8d3d2cd
Hugo Zul <hugoz1234@gmail.com>
2020-05-11 16:02:51 -0700

    first build

---
+++ content/posts/my-first-post.md
@@ -0,0 +1,6 @@
+---
+title: "My First Post"
+date: 2020-04-21T14:45:24-07:00
+draft: false
+---
+Hello World
---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# stop deployment if command fails
+set -e
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+# Build the project.
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+cd public
+
+git add .
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+
+git push origin master
